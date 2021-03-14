6152b425f303c0ff9223a69476bfc93ecab7fee5
John Moen <entity.john@gmail.com>
2021-03-14 09:45:26 -0500

    add create match to lobby - game setup  data exposed

--- src/NewLobby.tsx
+++ src/NewLobby.tsx
@@ -1,12 +1,6 @@
 import React from "react";
+import _ from "lodash";
 import { LobbyClient } from "boardgame.io/client";
-
-// const lobbyClient = new LobbyClient({ server: "http://localhost:8000" });
-
-// lobbyClient
-//   .listGames()
-//   .then(console.log) // => ['chess', 'tic-tac-toe']
-//   .catch(console.error);
 
 interface Props {
   serverAddress: string;
@@ -15,32 +9,30 @@ interface Props {
 export const NewLobby = (props: Props) => {
   const { serverAddress } = props;
   const [availableGames, setAvailableGames] = React.useState([]);
-  const [availableGamesError, setAvailableGamesError] = React.useState(
-    undefined
-  );
+  const [availableGamesError, setAvailableGamesError] = React.useState("");
   const [availableMatches, setAvailableMatches] = React.useState({});
   const [availableMatchesError, setAvailableMatchesError] = React.useState({});
+  const [createMatchError, setCreateMatchError] = React.useState("");
+  const [createMatchSuccess, setCreateMatchSuccess] = React.useState("");
   const lobbyClientRef = React.useRef(
     new LobbyClient({ server: `${serverAddress}` })
   );
-  const fireFetchAvailableGames = () => {
-    fetchAvailableGames();
-  };
-  const fireFetchAvailableMatches = (gameName: string) => {
-    fetchAvailableMatches(gameName);
-  };
+  // API CALLS
+  // GET matches
+  // GET games
   async function fetchAvailableGames() {
     const lobbyClient = lobbyClientRef.current;
     try {
       const games = await lobbyClient.listGames();
       if (games) {
-        setAvailableGamesError(undefined);
+        setAvailableGamesError("");
         setAvailableGames(games);
       }
     } catch (error) {
       setAvailableGamesError(error.message);
     }
   }
+  // GET matches
   async function fetchAvailableMatches(gameName: string) {
     const lobbyClient = lobbyClientRef.current;
     try {
@@ -50,20 +42,53 @@ export const NewLobby = (props: Props) => {
           ...s,
           [gameName]: undefined,
         }));
-        setAvailableMatches((s) => ({ ...s, [gameName]: matches }));
+        setAvailableMatches((s) => ({ ...s, [gameName]: matches.matches }));
       }
     } catch (error) {
       setAvailableMatchesError((s) => ({ ...s, [gameName]: error.message }));
     }
   }
-  // FETCH GAMES
+  // POST create match
+  async function createMatch(gameName: string, createGameOptions) {
+    const {
+      numPlayers = 2,
+      setupData = {},
+      unlisted = false,
+    } = createGameOptions;
+    const lobbyClient = lobbyClientRef.current;
+    try {
+      const response = await lobbyClient.createMatch(`${gameName}`, {
+        numPlayers,
+        setupData,
+        unlisted,
+      });
+      const newMatchID = response?.matchID;
+      if (newMatchID) {
+        fetchAvailableMatches(gameName);
+        setCreateMatchError("");
+        setCreateMatchSuccess(`${newMatchID}`);
+      }
+    } catch (error) {
+      setCreateMatchSuccess("");
+      setCreateMatchError(error.message);
+    }
+  }
+
+  // TODO
+  // Get Match by ID
+  // Join a Match
+  // Update Player Metadata
+  // Leave a Match
+  // Play Again
+
+  // ON MOUNT - fetch games
   React.useEffect(() => {
     const lobbyClient = lobbyClientRef.current;
     if (lobbyClient) {
       fetchAvailableGames();
     }
   }, []);
-  // FETCH MATCHES
+  // if games change, fetch matches for those games
   React.useEffect(() => {
     const lobbyClient = lobbyClientRef.current;
     if (lobbyClient) {
@@ -78,53 +103,136 @@ export const NewLobby = (props: Props) => {
       <GamesList
         availableGames={availableGames}
         availableGamesError={availableGamesError}
-        fireFetchAvailableGames={fireFetchAvailableGames}
+        fetchAvailableGames={fetchAvailableGames}
       />
       <MatchesList
         availableGames={availableGames}
         availableMatches={availableMatches}
         availableMatchesError={availableMatchesError}
-        fireFetchAvailableMatches={fireFetchAvailableMatches}
+        fetchAvailableGames={fetchAvailableGames}
+      />
+      <CreateGameForm
+        createMatch={createMatch}
+        availableGames={availableGames}
+        createMatchSuccess={createMatchSuccess}
+        createMatchError={createMatchError}
       />
     </div>
   );
 };
 
+function CreateGameForm({
+  createMatch,
+  availableGames,
+  createMatchSuccess,
+  createMatchError,
+}) {
+  const [gameSelect, setGameSelect] = React.useState(
+    availableGames?.[0] ?? undefined
+  );
+  const firstAvailableGame = availableGames?.[0];
+
+  // EFFECT: auto-select first game, once games are fetched
+  React.useEffect(() => {
+    if (firstAvailableGame && !gameSelect) {
+      setGameSelect(firstAvailableGame);
+    }
+  }, [availableGames, firstAvailableGame, gameSelect]);
+
+  const handleGameSelectChange = (e) => {
+    setGameSelect(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    createMatch(gameSelect, {
+      setupData: { player0Score: 0, player1Score: 100 },
+    });
+  };
+  const createMatchGameSelectHtmlID = `create-match-game-select`;
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h5>Create a New Match</h5>
+      <label htmlFor={createMatchGameSelectHtmlID}>
+        Choose game:
+        <select
+          onChange={handleGameSelectChange}
+          value={gameSelect}
+          id={createMatchGameSelectHtmlID}
+        >
+          {availableGames.map((gameName) => (
+            <option key={gameName} value={gameName}>
+              {gameName}
+            </option>
+          ))}
+        </select>
+      </label>
+      <div>
+        <button type="submit">CREATE GAME</button>
+        {createMatchError && (
+          <p style={{ color: "red" }}>
+            Error - Failed to create game: {`${createMatchError}`}
+          </p>
+        )}
+        {createMatchSuccess && (
+          <p style={{ color: "green" }}>
+            Created Match with ID: {`${createMatchSuccess}`}
+          </p>
+        )}
+      </div>
+    </form>
+  );
+}
+
 function MatchesList({
   availableGames,
   availableMatches,
   availableMatchesError,
-  fireFetchAvailableMatches,
+  fetchAvailableGames,
 }) {
-  console.dir(`🚀 ~ availableMatches`, availableMatches);
   if (availableGames.length > 0) {
     return availableGames.map((gameName) => (
-      <>
+      <section key={gameName}>
         <h3>{`Available Matches for ${gameName}`}</h3>
         {availableMatchesError[gameName] ? (
           <>
             <h4>Error</h4>
             <p>{`Unable to fetch matches for ${gameName} from server: ${availableMatchesError[gameName]}`}</p>
-            <button onClick={() => fireFetchAvailableMatches(gameName)}>
-              RETRY
-            </button>
+            <button onClick={() => fetchAvailableGames(gameName)}>RETRY</button>
           </>
         ) : (
-          <ul>
-            {/* {(availableMatches?.[gameName] || []).map((match) => (
-              <li>MATCH!</li>
-            ))} */}
-          </ul>
+          <>
+            <button onClick={() => fetchAvailableGames(gameName)}>
+              {`Refresh ${gameName} matches`}
+            </button>
+            <ul>
+              {_.uniqBy(availableMatches?.[gameName] ?? [], "matchID").map(
+                (match) => {
+                  const {
+                    createdAt,
+                    gameName,
+                    matchID,
+                    players,
+                    unlisted,
+                    updatedAt,
+                  } = match;
+                  return <li key={matchID}>{matchID}</li>;
+                }
+              )}
+            </ul>
+          </>
         )}
-      </>
+      </section>
     ));
   }
   return null;
 }
+
 function GamesList({
   availableGames,
   availableGamesError,
-  fireFetchAvailableGames,
+  fetchAvailableGames,
 }) {
   return (
     <>
@@ -133,12 +241,12 @@ function GamesList({
         <>
           <h4>Error</h4>
           <p>Unable to fetch games from server: {availableGamesError}</p>
-          <button onClick={fireFetchAvailableGames}>RETRY</button>
+          <button onClick={fetchAvailableGames}>RETRY</button>
         </>
       ) : (
         <ul>
-          {availableGames.map((game) => (
-            <li>{game}</li>
+          {availableGames.map((gameName) => (
+            <li key={gameName}>{gameName}</li>
           ))}
         </ul>
       )}
--- src/game/game.ts
+++ src/game/game.ts
@@ -1,9 +1,10 @@
 export const myGame = {
-  name: 'myGame',
+  name: "myGame",
   setup: (ctx, setupData) => {
     const myG = {
       player0Score: 0,
       player1Score: 0,
+      ...setupData,
     };
     return myG;
   },
